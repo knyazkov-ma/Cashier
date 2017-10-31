a45a4144487362880abe3376fadbeb3ed596d7b5
Князьков Максим <fix0007@mail.ru>
2017-10-31 19:45:54 +0500

    Исправление разметки

--- Cashier.App/FormAmount.Designer.cs
+++ Cashier.App/FormAmount.Designer.cs
@@ -31,13 +31,15 @@
             this.labelDocumentAmount = new System.Windows.Forms.Label();
             this.textBoxInput = new System.Windows.Forms.TextBox();
             this.labelCurrentName = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelDocumentAmount
             // 
             this.labelDocumentAmount.AutoSize = true;
             this.labelDocumentAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelDocumentAmount.Location = new System.Drawing.Point(197, 1);
+            this.labelDocumentAmount.Location = new System.Drawing.Point(195, 0);
             this.labelDocumentAmount.Name = "labelDocumentAmount";
             this.labelDocumentAmount.Size = new System.Drawing.Size(178, 55);
             this.labelDocumentAmount.TabIndex = 16;
@@ -46,9 +48,9 @@
             // textBoxInput
             // 
             this.textBoxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBoxInput.Location = new System.Drawing.Point(12, 65);
+            this.textBoxInput.Location = new System.Drawing.Point(14, 63);
             this.textBoxInput.Name = "textBoxInput";
-            this.textBoxInput.Size = new System.Drawing.Size(455, 80);
+            this.textBoxInput.Size = new System.Drawing.Size(449, 80);
             this.textBoxInput.TabIndex = 15;
             this.textBoxInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxInput_KeyDown);
             // 
@@ -56,20 +58,30 @@
             // 
             this.labelCurrentName.AutoSize = true;
             this.labelCurrentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelCurrentName.Location = new System.Drawing.Point(12, 13);
+            this.labelCurrentName.Location = new System.Drawing.Point(11, 8);
             this.labelCurrentName.Name = "labelCurrentName";
             this.labelCurrentName.Size = new System.Drawing.Size(126, 31);
             this.labelCurrentName.TabIndex = 14;
             this.labelCurrentName.Text = "К оплате";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.textBoxInput);
+            this.panel1.Controls.Add(this.labelDocumentAmount);
+            this.panel1.Controls.Add(this.labelCurrentName);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(479, 158);
+            this.panel1.TabIndex = 17;
             // 
             // FormAmount
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(479, 158);
-            this.Controls.Add(this.labelDocumentAmount);
-            this.Controls.Add(this.textBoxInput);
-            this.Controls.Add(this.labelCurrentName);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormAmount";
             this.ShowIcon = false;
@@ -78,8 +90,9 @@
             this.Text = "Закрытие чека";
             this.Load += new System.EventHandler(this.FormAmount_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormAmount_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -88,5 +101,6 @@
         private System.Windows.Forms.Label labelDocumentAmount;
         private System.Windows.Forms.TextBox textBoxInput;
         private System.Windows.Forms.Label labelCurrentName;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- Cashier.App/FormDifference.Designer.cs
+++ Cashier.App/FormDifference.Designer.cs
@@ -30,13 +30,15 @@
         {
             this.labelDifference = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelDifference
             // 
             this.labelDifference.AutoSize = true;
             this.labelDifference.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelDifference.Location = new System.Drawing.Point(143, 35);
+            this.labelDifference.Location = new System.Drawing.Point(144, 36);
             this.labelDifference.Name = "labelDifference";
             this.labelDifference.Size = new System.Drawing.Size(178, 55);
             this.labelDifference.TabIndex = 18;
@@ -46,19 +48,29 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Location = new System.Drawing.Point(11, 8);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(94, 31);
             this.label2.TabIndex = 17;
             this.label2.Text = "Сдача";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.labelDifference);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(479, 112);
+            this.panel1.TabIndex = 19;
             // 
             // FormDifference
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(479, 112);
-            this.Controls.Add(this.labelDifference);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormDifference";
             this.ShowIcon = false;
@@ -67,13 +79,15 @@
             this.Text = "Закрытие чека";
             this.Load += new System.EventHandler(this.FormDifference_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormDifference_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Label labelDifference;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
